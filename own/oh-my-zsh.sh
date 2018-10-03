a17e1aae0638c4cdfea9810a7e20e06fa141a6ad
KROSF <rodrigosanabria22@gmail.com>
2018-10-03 18:48:55 +0200

    ask for sudo

--- own/oh-my-zsh.sh
+++ own/oh-my-zsh.sh
@@ -8,7 +8,7 @@ GIST=2a8d6af7b7505488b14ec023a930590b
 print_in_purple "\n   Oh-My-Zsh\n\n"
 
 execute \
-        "sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"" \
+        "curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh && ./install.sh" \
         "Instalando"
 execute \
         "git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k" \
@@ -22,6 +22,7 @@ execute \
 execute \
         "cd $HOME sudo rm .zshrc && git clone https://gist.github.com/${GIST}.git && mv ${GIST}/.* ./ && sudo rm -r ${GIST}" \
         "Configuracion"
+ask_for_sudo
 execute \
         "chsh -s $(which zsh)" \
         "zsh como interprete predefinido"
