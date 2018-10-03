7fdee25eece404b533659c730ea5c3b8a6251a74
KROSF <rodrigosanabria22@gmail.com>
2018-10-03 19:10:49 +0200

    add unzip and cd to unzip maven

--- own/maven.sh
+++ own/maven.sh
@@ -10,5 +10,5 @@ execute \
         "cd /tmp && wget http://mirrors.koehn.com/apache/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.zip" \
         "Descargando"
 execute \
-        "unzip apache-maven-3.5.4-bin.zip && sudo mv apache-maven-3.5.4 /opt/apache-maven-3.5.4 && cd ${HOME}" \
+        "cd /tmp unzip apache-maven-3.5.4-bin.zip && sudo mv apache-maven-3.5.4 /opt/apache-maven-3.5.4 && cd ${HOME}" \
         "Instalando"
